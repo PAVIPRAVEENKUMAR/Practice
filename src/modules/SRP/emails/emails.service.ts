389f3eeb35ef78f0402df5c0d1ab42dc844efdb0
PAVIPRAVEENKUMAR <146061398+PAVIPRAVEENKUMAR@users.noreply.github.com>
2024-12-31 07:52:55 +0530

    Delete src/modules directory

--- src/modules/SRP/emails/emails.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class EmailsService {
-  public async sendOrderEmail(orderId: number) {
-    console.log(`Sending order email for order ${orderId}`);
-    
-  }
-}
