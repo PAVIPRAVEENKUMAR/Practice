4cd87f00565877e7eba0fe0ca95026a88b669ca5
PAVIPRAVEENKUMAR <146061398+PAVIPRAVEENKUMAR@users.noreply.github.com>
2025-01-02 12:53:34 +0530

    Delete test directory

--- test/src/modules/emails/emails.interface.service.ts
+++
@@ -1,4 +0,0 @@
-export interface IEmailService {
-    sendEmail(to: string, subject: string,  templates: string, body: string): Promise<void>;
-    getEmails(): Promise<any[]>;
-  }  
--- test/src/modules/products/products.service.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IProductService {
-    createProduct(productDto: { name: string; description: string; price: number }): Promise<any>;
-    findAllProducts(): Promise<any[]>;
-    findProductById(id: string): Promise<any>;
-    updateProduct(id: string, productDto: Partial<{ name: string; description: string; price: number }>): Promise<any>;
-    deleteProduct(id: string): Promise<any>;
-}  
